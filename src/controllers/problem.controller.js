b59a56c7f70ec6050ce7fe8e1ef06846f37ee3e0
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-03-30 00:34:06 +0530

    Feat: Modified controller for not implemented

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -13,28 +13,36 @@ function addProblem(req, res, next) {
     }
 }
 
-function getProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message: 'Not Implemented'
-    });
+function getProblem(req, res, next) {
+    try {
+        throw new NotImplemented('addProblem');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function getProblems(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message: 'Not Implemented'
-    });
+function getProblems(req, res, next) {
+    try {
+        throw new NotImplemented('addProblem');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function deleteProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message: 'Not Implemented'
-    });
+function deleteProblem(req, res, next) {
+    try {
+        throw new NotImplemented('addProblem');
+    } catch (error) {
+        next(error);
+    }
 }
 
-function updateProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message: 'Not Implemented'
-    });
+function updateProblem(req, res, next) {
+    try {
+        throw new NotImplemented('addProblem');
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
