b70ecf296446c4d0001c710fa157772a7fb07fec
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-04-08 01:14:44 +0530

    slight changed in server config file

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -4,5 +4,5 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT || 3000,
     ATLAS_DB_URL: process.env.ATLAS_DB_URL,
-    NODE_ENV: process.env.NODE_ENV || "developement"
+    NODE_ENV: process.env.NODE_ENV || "developement",
 };
